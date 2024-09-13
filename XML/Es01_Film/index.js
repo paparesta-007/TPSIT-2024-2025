fc70aa367dcbfd151fb3831bc005914180ca4a65
paparesta-007 <t.paparesta.3231@vallauri.edu>
2024-09-13 09:58:05 +0200

    Added Es01 Film

---
+++ XML/Es01_Film/index.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const films = [
+    // Id, Title, Favorite, Watch date, Rating (0-5)
+    [1, "Pulp Fiction", true, "10-03-2024", 5],
+    [2, "21 Grammi", true, "17-03-2024", 3],
+    [3, "Star Wars", false, "15-03-2024", 1],
+    [4, "Matrix", false, "01-01-2023", 4],
+    [5, "Shrek", false, "21-03-2024", 2],
+    [6, "Kill Bill Vol. 1", true, "22-04-2024", 5],
+    [7, "Inception", true, "18-04-2024", 5]
+];
+
+
+//window.addEventListener("load", function () {
+window.onload = function() {
+
+    
+
+}
