f219faaee923be9c8a23675b79a269aca84de892
paparesta-007 <t.paparesta.3231@vallauri.edu>
2024-09-24 11:39:29 +0200

    started ese04

--- XML/Ese04 Bookstore XML/index.js
+++ XML/Ese04 Bookstore XML/index.js
@@ -14,11 +14,30 @@ window.onload=function(){
 	addView.style.display="none"
 	const newDetail = addView.querySelector(".details")
 	
-
-
-
-
-
+	let xml=localStorage.getItem("bookstore_xml");
+	if(!xml)
+	{
+		xml=bookstore;	
+	}
+	const parser= new DOMParser;
+	let xmlDOC=parser.parseFromString(xml,"text/xml");
+	const xmlRoot=xmlDOC.documentElement;
+	loadBooks();
+	function loadBooks(){
+		divNbooks.textContent="Numeri di libri: "+xmlRoot.children.length; // solo figli diretti
+		let id="";
+		let title="";
+		let category="";
+		let year="";
+		let price="";
+		let language="";
+		let authors=""
+		for (const book of xmlRoot.children) {
+			if(book.hasAttribute(category)){
+				category=book.category;
+			}
+		}
+	}
 
 
 }
