12037f60881d0fc060e8163abf6610830c90b0cd
paparesta-007 <t.paparesta.3231@vallauri.edu>
2024-09-24 11:22:02 +0200

    added ese04 files

---
+++ XML/Ese04 Bookstore XML/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+window.onload=function(){
+    const listView = document.getElementById("list-view");
+	const divNbooks = document.querySelector(".nBooks")
+    const tBody = document.getElementById("tabLibri");
+	
+    const detailsView = document.getElementById("details-view");
+	detailsView.style.display="none"
+	const details = detailsView.querySelector(".details")
+	const spanCount = document.querySelector(".buttons span")
+
+    const addView = document.getElementById("add-view");
+	addView.style.display="none"
+	const newDetail = addView.querySelector(".details")
+	
+
+
+
+
+
+
+
+}
