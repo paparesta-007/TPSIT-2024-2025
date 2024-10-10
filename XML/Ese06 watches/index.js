b632730c2fe14de76a67ed37b21c4469db78798b
paparesta-007 <t.paparesta.3231@vallauri.edu>
2024-10-10 15:13:57 +0200

    Aggiustato carica orologi

--- XML/Ese06 watches/index.js
+++ XML/Ese06 watches/index.js
@@ -2,6 +2,8 @@
 let headers = ["Gender", "Code", "Price", "Color", "Image"];
 
 window.onload = function () {
+  
+
   let thead = document.querySelector("table thead");
   let tbody = document.querySelector("table tbody");
   let lstGender = document.querySelector(".gender select");
@@ -10,7 +12,11 @@ window.onload = function () {
   let genere = "All";
   select.addEventListener("change", function () {
     genere = this.value;
+
+    console.log(genere);
+    loadTable(genere);
   });
+  let code, price, color, image;
 
   let xml = localStorage.getItem("orologi_xml");
   if (!xml) {
@@ -25,14 +31,93 @@ window.onload = function () {
 
   //Creazione della tabella
   loadTable(genere);
+
   function loadTable(genere) {
     tbody.innerHTML = "";
+    thead.innerHTML = "";
     let tr = document.createElement("tr");
     thead.appendChild(tr);
+
     for (const header of headers) {
       let th = document.createElement("th");
       th.textContent = header;
       tr.appendChild(th);
     }
+
+    let orologi = xmlRoot.querySelectorAll("catalog_item");
+
+    for (const watch of orologi) {
+      let watchGender = watch.getAttribute("gender");
+
+      if (genere == "All" || genere == "" || watchGender == genere) {
+        let models = watch.querySelectorAll("model");
+
+        for (const model of models) {
+          // Leggo i dati per ogni modello
+          readWatches(model);
+          const tr = document.createElement("tr");
+          let td = document.createElement("td");
+          td.style.textAlign = "center";
+          td.textContent = watchGender;
+          tr.appendChild(td);
+
+          td = document.createElement("td");
+          td.style.textAlign = "center";
+          td.textContent = code;
+          tr.appendChild(td);
+
+          td = document.createElement("td");
+          td.style.textAlign = "center";
+          td.textContent = price;
+          tr.appendChild(td);
+
+          td = document.createElement("td");
+          td.style.textAlign = "center";
+          td.textContent = color;
+          tr.appendChild(td);
+
+          td = document.createElement("td");
+          td.style.textAlign = "center";
+          let img = document.createElement("img");
+          img.src = "./img/" + image;
+
+          img.alt = "Watch image";
+          img.width = 50; // Imposta la larghezza dell'immagine
+
+          td.appendChild(img);
+          tr.appendChild(td);
+
+          tbody.appendChild(tr);
+        }
+      }
+    }
   }
+
+  function readWatches(model) {
+    code = "";
+    image = "";
+    price = "";
+    color = "";
+
+    let codeNode = model.querySelector("code");
+    if (codeNode) {
+      code = codeNode.textContent;
+    }
+    let priceNode = model.querySelector("price");
+    if (priceNode) {
+      price = priceNode.textContent;
+    }
+
+    let colorNode = model.querySelector("color");
+    if (colorNode) {
+      color = colorNode.textContent;
+      image = colorNode.getAttribute("image");
+    }
+
+    console.log(code, price, color, image);
+  }
+
+  btnInserisci.addEventListener("click",function(){
+    window.open("inserisci.html","_blank");
+  })
 };
--- XML/Ese06 watches/inserisci.js
+++ XML/Ese06 watches/inserisci.js
@@ -1,12 +1,12 @@
 "use strict";
 
-window.onload=function(){
-	let _btnSalva = document.getElementById("btnSalva")
-	let _btnAnnulla = document.getElementById("btnAnnulla")	
-	let _txtCode = document.getElementById("txtCode")
-	let _txtPrice = document.getElementById("txtPrice")
-	let _lstColor = document.getElementById("lstColor")
-	let _optsGender = document.getElementsByName("optGender");
-	
+window.onload = function () {
+  let _btnSalva = document.getElementById("btnSalva");
+  let _btnAnnulla = document.getElementById("btnAnnulla");
+  let _txtCode = document.getElementById("txtCode");
+  let _txtPrice = document.getElementById("txtPrice");
+  let _lstColor = document.getElementById("lstColor");
+  let _optsGender = document.getElementsByName("optGender");
 
-}
+  
+};
