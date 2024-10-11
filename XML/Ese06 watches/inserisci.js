55b05ccdfa637eece43f472d0aac5c2b741d26f7
paparesta-007 <t.paparesta.3231@vallauri.edu>
2024-10-11 11:40:02 +0200

    continuo

--- XML/Ese06 watches/inserisci.js
+++ XML/Ese06 watches/inserisci.js
@@ -12,27 +12,46 @@ window.onload = function () {
     window.close();
   });
 
-  _btnSalva.addEventListener("click",function() {
-    if(_txtCode.value!="" && _txtPrice.value!=""){
-        let xml = localStorage.getItem("orologi_xml");
-        if (!xml) {
-          xml = orologi;
-          console.log("Successfully loaded orologi");
-        }
-        const parser = new DOMParser();
-        let xmlDoc = parser.parseFromString(xml, "text/xml");
-        let xmlRoot = xmlDoc.firstElementChild;
+  _btnSalva.addEventListener("click", function () {
+    if (_txtCode.value !== "" && _txtPrice.value !== "") {
+      // Recupera il file XML dal localStorage o utilizza l'oggetto `orologi` di default
+      let xml = localStorage.getItem("orologi_xml");
+      if (!xml) {
+        xml = orologi; // Variabile 'orologi' deve essere predefinita altrove nel codice
+        console.log("Successfully loaded orologi");
+      }
 
-        let gender=document.querySelector("input[type=radio]:checked").value;
-        console.log(gender)
-        xmlRoot.querySelector("catalog_item[gender="+gender+"]")
-    
+      const parser = new DOMParser();
+      let xmlDoc = parser.parseFromString(xml, "text/xml");
+      let xmlRoot = xmlDoc.firstElementChild;
+
+      // Selezioniamo il genere selezionato dal radio button
+      let gender = document.querySelector("input[type=radio]:checked").value;
+      console.log(gender);
+
+      // Troviamo il nodo corrispondente al catalog_item per il genere
+      let watchesGender = xmlRoot.querySelector("catalog_item[gender='" + gender + "']");
+
+      // Creiamo il modello usando una stringa template
+      let model = `
+      <model>
+        <code>${_txtCode.value}</code>
+        <price>${_txtPrice.value}</price>
+        <watches>
+          <color image="${_lstColor.value.toLowerCase()}_cardigan.jpg">${_lstColor.value}</color>
+        </watches>
+      </model>`;
+
+      watchesGender.appendChild(model);
     }
-    else{
-        alert("Inserisci tutti i")
+    else {
+      alert("Inserisci tutti i dati");
     }
-   
-  
+    let serializer = new XMLSerializer();
+      let newXml = serializer.serializeToString(xmlDoc);
+      localStorage.setItem("orologi_xml", newXml);
+
+
   })
-  
+
 };
