67dd895bc35a57610f092586bc8dbe409739405f
unknown <t.paparesta.3231@vallauri.net>
2024-10-15 10:31:07 +0200

    Finito ese 6

--- XML/Ese06 watches/index.js
+++ XML/Ese06 watches/index.js
@@ -1,101 +1,92 @@
 "use strict";
-let headers = ["Gender", "Code", "Price", "Color", "Image"];
+let headers = ["Gender", "Code", "Price", "Color", "Image"]
 
-window.onload = function () {
-  let thead = document.querySelector("table thead");
-  let tbody = document.querySelector("table tbody");
-  let lstGender = document.querySelector(".gender select");
-  let btnInserisci = document.getElementsByTagName("button")[0];
-  let select = document.querySelector("select");
+window.onload=function () {
+	let thead = document.querySelector("table thead")
+	let tbody = document.querySelector("table tbody")
+	let lstGender = document.querySelector(".gender select")
+	let btnInserisci = document.getElementsByTagName("button")[0]
 
-  let genere = "All";
-  select.addEventListener("change", function () {
-    genere = this.value;
-    console.log(genere);
-    loadTable(genere);
-  });
+	let xml = localStorage.getItem("watches_xml");
+	if(!xml)
+	{
+		xml = orologi;
+	}
+	const parser = new DOMParser();
+	const xmlDOC = parser.parseFromString(xml, "text/xml");
+	const xmlRoot = xmlDOC.firstElementChild;
 
-  let xml = localStorage.getItem("orologi_xml");
-  if (!xml) {
-    xml = orologi;
-    console.log("Successfully loaded orologi");
-  } else {
-    console.log("Error getting orologi");
-  }
-  const parser = new DOMParser();
-  let xmlDoc = parser.parseFromString(xml, "text/xml");
-  let xmlRoot = xmlDoc.firstElementChild;
-  
+	createHeaders();
+	loadTable("");
 
+	lstGender.addEventListener("change", function(){
+		loadTable(lstGender.value);
+	})
 
-  //Creazione della tabella
-  loadTable(genere);
-  function loadTable(genere) {
-    tbody.innerHTML = "";
-    thead.innerHTML = "";
-    let tr = document.createElement("tr");
-    thead.appendChild(tr);
-    for (const header of headers) {
-      let th = document.createElement("th");
-      th.textContent = header;
-      tr.appendChild(th);
-    }
+	btnInserisci.addEventListener("click", function(){
+		window.location.href = "./inserisci.html";
+	});
 
-    let watchlist=xmlRoot.querySelectorAll("catalog_item");
+	function createHeaders(){
+		const tr = document.createElement("tr");
+		thead.appendChild(tr);
 
-    for(const item of watchlist) {
-        let gender = item.getAttribute("gender");
-        let models=item.children;
-      
-        for(const model of models){
-         if (genere == "All" || gender == genere || genere == ""){
+		for (const header of headers) {
+			const th = document.createElement("th");
+			tr.appendChild(th);
+			th.textContent = header;
+		}
+	}
 
-         
-            let code = model.querySelector("code").textContent;
-            let price = model.querySelector("price").textContent;
-            let watches = model.querySelector("watches");
+	function loadTable(selectedGender){
+		tbody.innerHTML = "";
 
-            for (const colorWatch of watches.children) {
-              let color = colorWatch.textContent; 
-              let image = colorWatch.getAttribute("image");  
-          
-              let tr = document.createElement("tr");
-              tbody.appendChild(tr);
-          
-              let td = document.createElement("td");
-              td.style.textAlign = "center";
-              td.textContent = gender;
-              tr.appendChild(td);
-          
-              td = document.createElement("td");
-              td.textContent = code;
-              td.style.textAlign = "center";
-              tr.appendChild(td);
-          
-              td = document.createElement("td");
-              td.textContent = price;
-              td.style.textAlign = "center";
-              tr.appendChild(td);
-          
-              td = document.createElement("td");
-              td.textContent = color;  // Mostriamo il valore del colore corretto
-              td.style.textAlign = "center";
-              tr.appendChild(td);
-          
-              td = document.createElement("td");
-              let img = document.createElement("img");
-              img.src = "img/" + image;  // Ora viene mostrata l'immagine corretta
-              td.style.textAlign = "center";
-              td.appendChild(img);
-              tr.appendChild(td);
-          }
-          
-        }
-      }
+		let watchesList = xmlRoot.querySelectorAll("catalog_item");
 
-    }
-  }
-  btnInserisci.addEventListener("click", function () {
-    window.open("./inserisci.html","_blank");
-  })
-};
+		for (const item of watchesList) {
+			let gender = item.getAttribute("gender");
+
+			if(selectedGender == "" || gender == selectedGender)
+			{
+				//let models = item.querySelectorAll("model");
+				let models = item.children;
+				for (const model of models) {
+					let code = model.querySelector("code").textContent;
+					let price = model.querySelector("price").textContent;
+					let watches = model.querySelector("watches");
+
+					for (const colorWatch of watches.children) {
+						let color = colorWatch.textContent;
+						let image = colorWatch.getAttribute("image");
+
+						let tr = document.createElement("tr");
+						tbody.appendChild(tr);
+
+						let td = document.createElement("td");
+						tr.appendChild(td);
+						td.textContent = gender;
+
+						td = document.createElement("td");
+						tr.appendChild(td);
+						td.textContent = code;
+
+						td = document.createElement("td");
+						tr.appendChild(td);
+						td.textContent = price;
+
+						td = document.createElement("td");
+						tr.appendChild(td);
+						td.textContent = color;
+
+						td = document.createElement("td");
+						tr.appendChild(td);
+						const img = document.createElement("img");
+						img.src = "./img/" + image;
+						td.appendChild(img);
+					}
+				}	
+			}
+		}
+
+	}
+}
--- XML/Ese06 watches/inserisci.js
+++ XML/Ese06 watches/inserisci.js
@@ -1,57 +1,84 @@
 "use strict";
 
-window.onload = function () {
-  let _btnSalva = document.getElementById("btnSalva");
-  let _btnAnnulla = document.getElementById("btnAnnulla");
-  let _txtCode = document.getElementById("txtCode");
-  let _txtPrice = document.getElementById("txtPrice");
-  let _lstColor = document.getElementById("lstColor");
-  let _optsGender = document.getElementsByName("optGender");
+window.onload=function(){
+	let _btnSalva = document.getElementById("btnSalva")
+	let _btnAnnulla = document.getElementById("btnAnnulla")	
+	let _txtCode = document.getElementById("txtCode")
+	let _txtPrice = document.getElementById("txtPrice")
+	let _lstColor = document.getElementById("lstColor")
+	//let _optsGender = document.getElementsByName("optGender");
+	
+	_btnAnnulla.addEventListener("click", function(){
+		window.location.href = "./index.html";
+	});
 
-  _btnAnnulla.addEventListener("click", function () {
-    window.close();
-  });
+	_btnSalva.addEventListener("click", function(){
+		if(_txtCode.value != "" && _txtPrice.value != "")
+		{
+			let xml = localStorage.getItem("watches_xml");
+			if(!xml)
+			{
+				xml = orologi;
+			}
+			const parser = new DOMParser();
+			const xmlDOC = parser.parseFromString(xml, "text/xml");
+			const xmlRoot = xmlDOC.firstElementChild;
+			
+			let gender = document.querySelector("input[type=radio]:checked").value; //pseudoselettore
+			console.log(gender);
 
-  _btnSalva.addEventListener("click", function () {
-    if (_txtCode.value !== "" && _txtPrice.value !== "") {
-      // Recupera il file XML dal localStorage o utilizza l'oggetto `orologi` di default
-      let xml = localStorage.getItem("orologi_xml");
-      if (!xml) {
-        xml = orologi; // Variabile 'orologi' deve essere predefinita altrove nel codice
-        console.log("Successfully loaded orologi");
+			let watchesGender = xmlRoot.querySelector(`catalog_item[gender="${gender}"]`);
+
+
+      let trovato=false;
+      for(const model of watchesGender.children) {
+        if(model.querySelector("code").textContent==_txtCode.value){
+          trovato=!trovato;
+          alert("Codice già presente");
+          let watches = model.querySelector("watches")
+          const color=document.createElement("color");
+          color.textContent=_lstColor.value;
+          color.setAttribute("image", `${_lstColor.value.toLowerCase()}_cardigan.jpg`);
+          watches.appendChild(color);
+     
+        }
+        
       }
+      if(!trovato){
+        let model = `		
+        <model>
+          <code>${_txtCode.value}</code>
+          <price>${_txtPrice.value}</price>
+          <watches>
+            <color image="${_lstColor.value.toLowerCase()}_cardigan.jpg">${_lstColor.value}</color>
+          </watches>
+        </model>`;
+  
+        //soluzione migliore
+        const modelObj = parser.parseFromString(model, "text/xml");
+        console.log(modelObj);
+  
+        watchesGender.appendChild(modelObj.firstElementChild);
+  
+        //watchesGender.innerHTML += model;
+      }
+      
+      
 
-      const parser = new DOMParser();
-      let xmlDoc = parser.parseFromString(xml, "text/xml");
-      let xmlRoot = xmlDoc.firstElementChild;
+			let serializer = new XMLSerializer();
+			let xmlStr = serializer.serializeToString(xmlDOC);
+			localStorage.setItem("watches_xml", xmlStr);
 
-      // Selezioniamo il genere selezionato dal radio button
-      let gender = document.querySelector("input[type=radio]:checked").value;
-      console.log(gender);
+			alert("Record aggiunto correttamente");
+			window.location.href = "index.html";
 
-      // Troviamo il nodo corrispondente al catalog_item per il genere
-      let watchesGender = xmlRoot.querySelector("catalog_item[gender='" + gender + "']");
+			//non funziona perchè è una stringa e .appendChild si aspetta un puntatore
+			//watchesGender.appendChild(model); 
+		}
+		else
+		{
+			alert("Compila tutti i campi!");
+		}
+	});
 
-      // Creiamo il modello usando una stringa template
-      let model = `
-      <model>
-        <code>${_txtCode.value}</code>
-        <price>${_txtPrice.value}</price>
-        <watches>
-          <color image="${_lstColor.value.toLowerCase()}_cardigan.jpg">${_lstColor.value}</color>
-        </watches>
-      </model>`;
-
-      watchesGender.appendChild(model);
-    }
-    else {
-      alert("Inserisci tutti i dati");
-    }
-    let serializer = new XMLSerializer();
-      let newXml = serializer.serializeToString(xmlDoc);
-      localStorage.setItem("orologi_xml", newXml);
-
-
-  })
-
-};
+}
