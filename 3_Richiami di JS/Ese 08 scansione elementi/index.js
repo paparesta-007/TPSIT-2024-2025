8ad43dc4aa59c18f37ab6b509eb5f000220f3e10
unknown <t.paparesta.3231@vallauri.net>
2024-10-22 11:03:32 +0200

    Finito esercizio 8, aggiunti file sercizio 9

--- 3_Richiami di JS/Ese 08 scansione elementi/index.js
+++ 3_Richiami di JS/Ese 08 scansione elementi/index.js
@@ -6,24 +6,30 @@ window.onload = function () {
   wrapper = document.querySelector("#wrapper");
   let buttonsDiv = document.querySelector("#buttons");
   let btns = buttonsDiv.querySelectorAll("input[type=button]");
-  let btnContaNElementi=buttonsDiv.getElementsByTagName("input")[0];
+  let btnContaNElementi = buttonsDiv.getElementsByTagName("input")[0];
   let btnColoraIndice = buttonsDiv.getElementsByTagName("input")[3]; // Usa input al posto di button
 
-  btnContaNElementi.addEventListener("click",function(){
-	let n = wrapper.children.length;
-	alert(`Ci sono ${n} elementi`)
+  btnContaNElementi.addEventListener("click", function () {
+    let n = wrapper.children.length;
+    alert(`Ci sono ${n} elementi`)
   })
-  
-  btnColoraIndice.addEventListener("click", function () {
-    let i = 0;
 
-    for (const item of wrapper.children) {
-      if (i % 2 === 1) {
-        item.style.backgroundColor = `rgb(0, ${50 * (i + 1)}, 0)`;
-      }
-        i++;
-    }
+  btnColoraIndice.addEventListener("click", function () {
+    let green=50;
+    let oddElement=wrapper.querySelectorAll("li:nth-of-type(odd)")
+  for(const element of oddElement){
+
+      element.style.backgroundColor="rgb(0,"+green+",0)";
+      green+=50
+  }
+    // for (const item of wrapper.children) {
+    //   if (item.matches(":nth-of-type(odd)")) {
+    //     item.style.backgroundColor = `rgb(0, ${50 * (i + 1)}, 0)`;
+    //   }
+    //     i++;
+    // }
   });
+  
 };
 
 function evidenzia(selector) {
---
+++ 3_Richiami di JS/Ese 09 Form e Controlli/index.js
@@ -0,0 +1,25 @@
+'use strict'
+let form1;
+
+window.onload = function() {
+	form1 = document.getElementById("form1")
+}
+
+// richiamato dall'html
+function visualizza(index) {
+	let msg = "";
+	let items;
+	switch (index) {
+
+	}
+	alert(msg);
+}
+
+
+function imposta(index){
+	let items	
+	switch(index){
+
+	}	 
+}
+
