9355b1207530a7f13ee2d0ed15884af7f0cec3e9
paparesta-007 <t.paparesta.3231@vallauri.edu>
2024-11-04 15:53:57 +0100

    Aggiunti file esercizio 10 bookstore JSON

---
+++ 4_JSON/Ese 10 bookstore JSON/bookstore.js
@@ -0,0 +1,42 @@
+let bookstore = `[
+	{
+		"id" : "1",
+		"title" : "Everyday Italian", 
+		"category" : "cooking",
+		"lang" : "en", 
+		"authors" : ["Giada De Laurentiis"],
+		"year" : "2005",
+		"price" : "30.00"
+	},				
+	{
+		"id" : "2",
+		"title" : "Harry Potter", 
+		"category" : "cooking",
+		"lang" : "en", 
+		"authors" : ["J K. Rowling"],
+		"year" : "",
+		"price" : "29.99"
+	},				
+	{
+		"id" : "3",
+		"title" : "XQuery Kick Start", 
+		"category" : "web",
+		"lang" : "", 
+		"authors" : ["James McGovern", "Per Bothner", "Kurt Cagle", "James Linn", "Vaidyanathan Nagarajan"],
+		"year" : "2003",
+		"price" : "49.99"
+	},				
+	{
+		"id" : "4",
+		"title" : "Learning XML", 
+		"category" : "web",
+		"lang" : "en", 
+		"authors" : ["Erik T. Ray"],
+		"year" : "2003",
+		"price" : "39.95"
+	}				
+]`
+	
+	
+	
+	
