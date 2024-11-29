cc5e60881d80c93108a42f810d6621374656efd8
unknown <t.paparesta.3231@vallauri.net>
2024-11-29 11:39:47 +0100

    aggiunto file jquery esercio goal

---
+++ 6_JQuery/Ese 01 Goal/index.js
@@ -0,0 +1,21 @@
+"use strict"
+$(document).ready(function()
+{	
+	let calciatore = $("#calciatore");
+	let palla = $("#palla");
+	
+	let btnEntra=$("#btnEntra")
+	let btnEsci = $("#btnEsci")
+	let btnVisualizzaPalla = $("#btnVisualizzaPalla")
+	let btnNascondiPalla = $("#btnNascondiPalla")
+	let btnTira = $("#btnTira")
+
+	
+	
+	
+	
+});
+
+
+
+
