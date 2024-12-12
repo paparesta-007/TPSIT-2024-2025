a5d7e652ad33486646674fca471ad3c4e3c3738f
unknown <t.paparesta.3231@vallauri.net>
2024-12-12 15:37:35 +0100

    continuato froza 4

--- 6_JQuery/Ese 07 Forza 4/index.js
+++ 6_JQuery/Ese 07 Forza 4/index.js
@@ -5,52 +5,65 @@ const GIALLO = "#FF0";
 const ROSSO = "#F00";
 let turno = GIALLO;
 
+// Initialize an empty board
+let board = [];
+for (let i = 0; i < RIGHE; i++) {
+  board[i] = [];
+  for (let j = 0; j < COLONNE; j++) {
+    board[i][j] = null; // null means the cell is empty
+  }
+}
+
 $(function () {
-    let header = $("#header");
-    let wrapper = $("#wrapper");
+  let header = $("#header");
+  let wrapper = $("#wrapper");
 
-    creaPedine();
-    creaPedineIntestazione();
+  creaPedine();
+  creaPedineIntestazione();
 
-    function creaPedineIntestazione() {
-        for (let i = 0; i < COLONNE; i++) {
-            let div = $("<div>", {
-                "class": "pedina",
-                "appendTo": header,
-                "on": {
-                    "mouseover": function () {
-                        div.css({
-                            backgroundColor: turno == GIALLO ? "yellow" : "red"
-                        });
-                    },
-                    "mouseout": function () {
-                        div.css({
-                            backgroundColor: "#BBB"
-                        });
-                    },
-                    "click": scendi(i) ,
-                }
+  // Create header tokens (where players click to drop tokens)
+  function creaPedineIntestazione() {
+    for (let i = 0; i < COLONNE; i++) {
+      let div = $("<div>", {
+        "class": "pedina",
+        "appendTo": header,
+        "on": {
+          "mouseover": function () {
+            div.css({
+              backgroundColor: turno == GIALLO ? GIALLO : ROSSO
             });
+          },
+          "mouseout": function () {
+            div.css({
+              backgroundColor: "#BBB"
+            });
+          },
+          "click": function () {
+            // Drop the piece in the clicked column
+            scendi(i);
+            // Toggle player turn
+            turno = (turno == GIALLO) ? ROSSO : GIALLO;
+          }
         }
+      });
     }
+  }
 
-    function creaPedine() {
-        for (let i = 0; i < COLONNE; i++) {
-            for (let j = 0; j < RIGHE; j++) {
-                let div = $("<div>", {
-                    "class": "pedina",
-                    "appendTo": wrapper,
-                    "id": i + "-" + j
-                });
-            }
-        }
+  // Function to simulate a piece falling down a column
+  function scendi(colonna) {
+
+  }
+
+  // Function to create the grid of tokens (board layout)
+  function creaPedine() {
+    for (let i = 0; i < COLONNE; i++) {
+      for (let j = 0; j < RIGHE; j++) {
+        let div = $("<div>", {
+          "class": "pedina",
+          "appendTo": wrapper,
+          "id": i + "-" + j // Set the id as the column-row (e.g., 0-0, 0-1, etc.)
+        });
+      }
     }
-
-	function scendi(i){
-		// Change the player's turn
-		turno = turno == GIALLO ? ROSSO : GIALLO;
-                        
-		// Set the background color of the column header to reflect the current player's turn
-		
-	}
+  }
 });
