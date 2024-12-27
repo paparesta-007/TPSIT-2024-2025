f924924ef04c5c853c9e69d8b9249c2e890b17b1
Ghismonda Boccaccio <t.paparesta.3231@vallauri.edu>
2024-12-27 09:40:22 +0100

    finito ese 03 labirinto

--- 6_JQuery/Ese 03 Labirinto/index.js
+++ 6_JQuery/Ese 03 Labirinto/index.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
 
-    var _btnAvvia = $("#btnAvvia");
+    let _btnAvvia = $("#btnAvvia");
 	_btnAvvia.on("click", eseguiAnimazione);
- 
+	
+	function flash(){
+		_btnAvvia.animate({"opacity": 0}, 450, function(){
+			_btnAvvia.animate({"opacity": 1}, 450, flash);
+		});
+	}
+	flash()
 	function eseguiAnimazione(){ 
+		_btnAvvia.hide();
 		$("#pedina")		
-		.css({"left":"10px","top":"260px", "width":"15px", "height":"15px"})
-		.animate({"left":'+=60px', "width":"8px",  "height":"8px"}, 1300)
+		.css({"left":"10px","top":"260px", "width":"15px", "height":"15px"},)
+		.animate({"left":'+=60px', "width":"8px",  "height":"8px"}, 1300,)
 		.animate({"top":'+=38px',  "width":"15px", "height":"15px"},1300)
 		.animate({"left":'+=116px',"width":"8px",  "height":"8px"}, 1300)
 		.animate({"top":'+=77px',  "width":"15px", "height":"15px"},1300)
-		.animate({"left":'+=250px',"width":"8px",  "height":"8px"}, 1300)
+		.animate({"left":'+=250px',"width":"8px",  "height":"8px"}, 1300,riattiva)
+		
 	}
-	
+	function riattiva(){
+		_btnAvvia.show();
+	}
 });
