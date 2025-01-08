ff38ea7c4bc201503078016ee00bcddb5bb067a9
paparesta-007 <t.paparesta.3231@vallauri.edu>
2025-01-08 09:46:50 +0100

    aggiunto file ese 02 ajax

---
+++ 5_Ajax/ese 02 Automobili/index.js
@@ -0,0 +1,18 @@
+"use strict"
+
+$(document).ready(function () {
+	
+	let _lstMarche = $("#lstMarche");
+	let _lstModelli = $("#lstModelli");
+	let _table= $("table")
+	let _dettagli=$(".details")
+
+	_table.hide()
+	_dettagli.hide()
+
+       
+
+		
+});
+
+
