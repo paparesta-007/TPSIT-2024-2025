05e791e8532c40ac9ad5fe4be6771a9b6a8feeb2
unknown <t.paparesta.3231@vallauri.net>
2025-02-04 11:41:08 +0100

    continuato ese04 parte base64

--- 5_Ajax/Ese04 Quadri/index.js
+++ 5_Ajax/Ese04 Quadri/index.js
@@ -19,25 +19,25 @@ $(function () {
         let artisti = response.data;
 
         artisti.forEach((artista, index) => {
-            
+
 
             const input = document.createElement("input");
             input.type = "radio";
             input.name = "artista";
             input.value = artista.id;
-            input.dataset.artista = JSON.stringify(artista); 
+            input.dataset.artista = JSON.stringify(artista);
             const lbl = document.createElement("label");
-            lbl.innerHTML = artista.name; 
-            
+            lbl.innerHTML = artista.name;
+
             if (index == 0) {
-                input.checked = true; 
+                input.checked = true;
             }
-            
-            lbl.insertBefore(input, lbl.firstChild); 
+
+            lbl.insertBefore(input, lbl.firstChild);
             _head.appendChild(lbl);
             input.addEventListener("change", function () {
                 let artistaSelezionato = JSON.parse(this.dataset.artista);
-                
+
                 console.log(artistaSelezionato);
                 getQuadri(artistaSelezionato);
             });
@@ -55,7 +55,7 @@ $(function () {
 
     async function visualizzaQuadro(pos) {
         let quadro = quadri[indiceQuadro];
-        _info.innerHTML = ""; 
+        _info.innerHTML = "";
 
         let div = document.createElement("div");
         _info.appendChild(div);
@@ -74,38 +74,38 @@ $(function () {
         div = document.createElement("div");
         _info.appendChild(div);
         div.innerHTML = `Like: <b>${quadro.nLike}</b>`;
-        let imgLike=document.createElement("img");
-        imgLike.src="./img/like.jpg";
-        imgLike.style.width="30px";
-        imgLike.style.cursor="pointer"
-        imgLike.addEventListener("click",function(){
+        let imgLike = document.createElement("img");
+        imgLike.src = "./img/like.jpg";
+        imgLike.style.width = "30px";
+        imgLike.style.cursor = "pointer"
+        imgLike.addEventListener("click", function () {
             aggiungiLike(quadro)
         })
         div.appendChild(imgLike);
 
-        _img.innerHTML = ""; 
+        _img.innerHTML = "";
         let img = document.createElement("img");
-        if(!quadro.img.startsWith("data:image/")){
-            
+        if (!quadro.img.startsWith("data:image/")) {
+
             img.src = `./img/${quadro.img}`;
         }
-        else{
+        else {
             img.src = quadro.img;
         }
         _img.appendChild(img);
     }
-    _btnPrev.addEventListener("click",function() {
+    _btnPrev.addEventListener("click", function () {
         if (indiceQuadro > 0) {
             indiceQuadro--;
             visualizzaQuadro();
         }
-    
+
         if (indiceQuadro === 0) {
             _btnPrev.disabled = true;
         }
         _btnNext.disabled = false;
     })
-    _btnNext.addEventListener("click", function() {
+    _btnNext.addEventListener("click", function () {
         if (indiceQuadro < quadri.length - 1) {
             indiceQuadro++;
             visualizzaQuadro();
@@ -117,14 +117,46 @@ $(function () {
     })
 
     function aggiungiLike(quadro) {
-        let path="/quadri/"+quadro.id
-        let response=inviaRichiesta("PATCH",path,{"nLike":(quadro.nLike+1)})
+        let path = "/quadri/" + quadro.id
+        let response = inviaRichiesta("PATCH", path, { "nLike": (quadro.nLike + 1) })
         response.catch(errore);
-        response.then(function(response){
+        response.then(function (response) {
             console.log(response)
             visualizzaQuadro(quadro)
         })
-      }
+    }
+
+    btnSalva.addEventListener("click", function () {
+        let blob = txtFile.files[0];
+        let title = txtTitolo.value;
+        if (!blob || !title) {
+            alert("Compila tutti i campi!")
+            return;
+        }
+        else {
+            let promise = base64Convert(blob);
+            promise.catch(function (err) {
+                alert("Errore con la conversione da Blob a Base64: " + err);
+                reject(error);
+            })
+            promise.then(function (base64) {
+            
+            })
+        }
+    });
+    function base64Convert(blob) {
+        return new Promise(function (resolve, reject) {
+            let reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onload = function (event) {
+                resolve(event.target.result); // event.target sarebbe reader
+            };
+            reader.onerror = function (error) {
+                reject(error);
+            };
+        })
+    }
+
 });
 
 function random(min, max) {
