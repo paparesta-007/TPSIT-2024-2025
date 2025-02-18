83f65b52b648ef294e5ddf1d1111788c92e24140
Ghismonda Boccaccio <t.paparesta.3231@vallauri.edu>
2025-02-18 20:33:03 +0100

    aggiustato ese04 quadro base64

--- 5_Ajax/Ese04 Quadri/index.js
+++ 5_Ajax/Ese04 Quadri/index.js
@@ -1,164 +1,198 @@
 "use strict";
 
 let quadri = [];
+let checkedArtist;
+let indexPainting = 0;
+
 $(function () {
-    let _head = $(".head").get(0);
-    let _info = document.querySelector(".info");
-    let _img = document.querySelector(".img");
-    let _btnPrev = $("button").get(0);
-    let _btnNext = $("button").get(1);
-    let _wrapperAdd = $(".wrapper").get(0);
+  let _head = $(".head")[0];
+  let _info = $(".info")[0];
+  let _img = $(".img")[0];
+  let _btnPrev = $("button")[0];
+  let _btnNext = $("button")[1];
+  let _wrapperAdd = $(".wrapper")[1];
 
-    let indiceQuadro = 0;
-    _btnPrev.disabled = true;
+  _btnPrev.disabled = true;
 
-    getArtists();
+  getArtists();
 
-    async function getArtists() {
-        let response = await inviaRichiesta("GET", "/artisti").catch(errore);
-        let artisti = response.data;
+  let btns = $(_btnPrev).add($(_btnNext));
 
-        artisti.forEach((artista, index) => {
+  btns.on("click", function () {
+    if (this.textContent.trim() == "Avanti") indexPainting++;
+    else indexPainting--;
 
+    statusBtns();
 
-            const input = document.createElement("input");
-            input.type = "radio";
-            input.name = "artista";
-            input.value = artista.id;
-            input.dataset.artista = JSON.stringify(artista);
-            const lbl = document.createElement("label");
-            lbl.innerHTML = artista.name;
+    loadPaintings(checkedArtist);
+  });
 
-            if (index == 0) {
-                input.checked = true;
-            }
+  async function getArtists() {
+    let response = await inviaRichiesta("GET", "/artisti").catch(errore);
 
-            lbl.insertBefore(input, lbl.firstChild);
-            _head.appendChild(lbl);
-            input.addEventListener("change", function () {
-                let artistaSelezionato = JSON.parse(this.dataset.artista);
+    let artists = response.data;
 
-                console.log(artistaSelezionato);
-                getQuadri(artistaSelezionato);
-            });
-        });
-
-        getQuadri(artisti[0]);
+    for (const artist of artists) {
+      const lbl = document.createElement("label");
+      _head.appendChild(lbl);
+      const input = document.createElement("input");
+      input.type = "radio";
+      input.name = "artisti";
+      input.setAttribute("data-artista", JSON.stringify(artist)); //solo stringhe quindi convertire in stringa il JSON
+      input.addEventListener("click", function () {
+        indexPainting = 0;
+        statusBtns();
+        checkedArtist = JSON.parse(this.getAttribute("data-artista"));
+        loadPaintings(checkedArtist);
+      });
+      lbl.appendChild(input);
+      //riscrive il contenuto dell'oggetto label e quindi gli oggetti interni perdono sia gli eventi sia gli attributi personalizzati
+      // lbl.innerHTML += artista.name;
+      const span = document.createElement("span");
+      span.textContent = artist.name;
+      lbl.appendChild(span);
     }
 
-    async function getQuadri(artista) {
-        let response = await inviaRichiesta("GET", "/quadri/", { artist: artista.id }).catch(errore);
-        quadri = response.data;
-        console.log(quadri);
-        visualizzaQuadro();
-    }
+    let nArtista = random(0, artists.length);
 
-    async function visualizzaQuadro(pos) {
-        let quadro = quadri[indiceQuadro];
-        _info.innerHTML = "";
+    checkedArtist = JSON.parse(
+      _head
+        .querySelectorAll("input[type=radio]")
+        [nArtista].getAttribute("data-artista")
+    );
+    //console.log(checkedArtist);
+    _head.querySelectorAll("input[type=radio]")[nArtista].checked = true;
 
-        let div = document.createElement("div");
-        _info.appendChild(div);
-        div.innerHTML = `ID= <b>${quadro.id}</b>`;
+    loadPaintings(checkedArtist);
+  }
 
-        div = document.createElement("div");
-        _info.appendChild(div);
-        div.innerHTML = `Titolo: <b>${quadro.title}</b>`;
+  async function loadPaintings(artista) {
+    //console.log(artista);
 
-        let artistaSelezionato = JSON.parse(_head.querySelector('input[type="radio"]:checked').dataset.artista);
+    let response = await inviaRichiesta("GET", "/quadri", {
+      artist: artista.id,
+    }).catch(errore);
 
-        div = document.createElement("div");
-        _info.appendChild(div);
-        div.innerHTML = `Genere: <b>${artistaSelezionato.gender}</b>`;
+    quadri = response.data;
 
-        div = document.createElement("div");
-        _info.appendChild(div);
-        div.innerHTML = `Like: <b>${quadro.nLike}</b>`;
-        let imgLike = document.createElement("img");
-        imgLike.src = "./img/like.jpg";
-        imgLike.style.width = "30px";
-        imgLike.style.cursor = "pointer"
-        imgLike.addEventListener("click", function () {
-            aggiungiLike(quadro)
-        })
-        div.appendChild(imgLike);
+    //console.log(quadri);
 
-        _img.innerHTML = "";
-        let img = document.createElement("img");
-        if (!quadro.img.startsWith("data:image/")) {
+    showPaintings();
+  }
 
-            img.src = `./img/${quadro.img}`;
-        }
-        else {
-            img.src = quadro.img;
-        }
-        _img.appendChild(img);
-    }
-    _btnPrev.addEventListener("click", function () {
-        if (indiceQuadro > 0) {
-            indiceQuadro--;
-            visualizzaQuadro();
-        }
+  async function showPaintings() {
+    let quadro = quadri[indexPainting];
 
-        if (indiceQuadro === 0) {
-            _btnPrev.disabled = true;
-        }
-        _btnNext.disabled = false;
-    })
-    _btnNext.addEventListener("click", function () {
-        if (indiceQuadro < quadri.length - 1) {
-            indiceQuadro++;
-            visualizzaQuadro();
-        }
-        if (indiceQuadro === quadri.length - 1) {
-            _btnNext.disabled = true;
-        }
-        _btnPrev.disabled = false;
-    })
+    _info.innerHTML = "";
+    _img.innerHTML = "";
 
-    function aggiungiLike(quadro) {
-        let path = "/quadri/" + quadro.id
-        let response = inviaRichiesta("PATCH", path, { "nLike": (quadro.nLike + 1) })
-        response.catch(errore);
-        response.then(function (response) {
-            console.log(response)
-            visualizzaQuadro(quadro)
-        })
-    }
+    //INFO
+    let div = document.createElement("div");
+    div.innerHTML = `ID = <b>${quadro.id}</b>`;
+    _info.appendChild(div);
 
-    btnSalva.addEventListener("click", function () {
-        let blob = txtFile.files[0];
-        let title = txtTitolo.value;
-        if (!blob || !title) {
-            alert("Compila tutti i campi!")
-            return;
-        }
-        else {
-            let promise = base64Convert(blob);
-            promise.catch(function (err) {
-                alert("Errore con la conversione da Blob a Base64: " + err);
-                reject(error);
-            })
-            promise.then(function (base64) {
-            
-            })
-        }
+    div = document.createElement("div");
+    div.innerHTML = `Titolo = <b>${quadro.title}</b>`;
+    _info.appendChild(div);
+
+    div = document.createElement("div");
+    div.innerHTML = `Genere = <b>${checkedArtist.gender}</b>`;
+    _info.appendChild(div);
+
+    div = document.createElement("div");
+    const span = document.createElement("span");
+    span.innerHTML = `Like = <b>${quadro.nLike}</b>`;
+    span.id = "nLike";
+    div.appendChild(span);
+    const imgLike = document.createElement("img");
+    imgLike.src = "./img/like.jpg";
+    imgLike.classList.add("like");
+    imgLike.addEventListener("click", () => {
+      quadro.nLike++;
+      document.getElementById(
+        "nLike"
+      ).innerHTML = `Like = <b>${quadro.nLike}</b>`;
+      let request = inviaRichiesta("PATCH", "/quadri/" + quadro.id, {
+        nLike: quadro.nLike,
+      });
+      request.catch(errore);
+      request.then(function (response) {
+        alert("Hai messo un piace a questo quadro");
+      });
     });
-    function base64Convert(blob) {
-        return new Promise(function (resolve, reject) {
-            let reader = new FileReader();
-            reader.readAsDataURL(blob);
-            reader.onload = function (event) {
-                resolve(event.target.result); // event.target sarebbe reader
-            };
-            reader.onerror = function (error) {
-                reject(error);
-            };
-        })
-    }
+    div.appendChild(imgLike);
+    _info.appendChild(div);
 
+    //IMG
+    const img = document.createElement("img");
+    img.style.width = "200px";
+    //se l'immagine non è base64
+    if (!quadro.img.startsWith("data:image/")) img.src = `./img/${quadro.img}`;
+    //se l'immagine è base64
+    else img.src = quadro.img;
+    _img.appendChild(img);
+  }
+
+  function statusBtns() {
+    if (indexPainting <= 0) _btnPrev.disabled = true;
+    else _btnPrev.disabled = false;
+
+    if (indexPainting >= quadri.length - 1) _btnNext.disabled = true;
+    else _btnNext.disabled = false;
+  }
+
+  btnSalva.addEventListener("click", function () {
+    let blob = txtFile.files[0];
+    let title = txtTitolo.value;
+
+    if (!title || !blob) alert("Scegliere un file e assegnare un titolo");
+    else {
+      let promise = base64Convert(blob);
+
+      promise.catch(function (err) {
+        alert("Errore conversione immagine: \n" + err);
+      });
+      promise.then(function (base64Img) {
+        let artist = JSON.parse(
+          _head
+            .querySelector("input[type=radio]:checked")
+            .getAttribute("data-artista")
+        );
+        console.log(artist.id);
+
+        let quadro = {
+          artist: artist.id,
+          title: title,
+          img: base64Img,
+          nLike: 0,
+        };
+
+        let request = inviaRichiesta("POST", "/quadri", quadro);
+
+        request.catch(errore);
+        request.then(function (HTTPResponse) {
+          console.log(HTTPResponse);
+          alert("Quadro inserito correttamente");
+          showPaintings();
+        });
+      });
+    }
+  });
 });
 
 function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor((max - min) * Math.random()) + min;
+}
+
+function base64Convert(blob) {
+  return new Promise(function (resolve, reject) {
+    let reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = function (event) {
+      resolve(event.target.result); // event.target sarebbe reader
+    };
+    reader.onerror = function (error) {
+      reject(error);
+    };
+  });
 }
